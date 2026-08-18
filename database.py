b2a877ce1cf451a80d1b893f8c55740fbc63b123
Prashant Gawai <gawaip043@gmail.com>
2026-08-18 16:52:07 +0530

    make databse.py resilient to missing DATABASE_URL for CI

--- database.py
+++ database.py
@@ -4,11 +4,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)  # the actual connection to Neon postgres
-SessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine
-)  # how Fast api will open and close individual DB conversations per request
 Base = declarative_base()  # the parent class of your table models
+
+engine = None  # the actual connection to Neon postgres
+SessionLocal = None
+
+if DATABASE_URL:
+    engine = create_engine(DATABASE_URL)
+    SessionLocal = sessionmaker(
+        autocommit=False, autoflush=False, bind=engine
+    )  # how Fast api will open and close individual DB conversations per request
